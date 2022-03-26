734028e153e1ed2bad156365f0965cc7a730babb
Pranav-Talmale <67180305+Pranav-Talmale@users.noreply.github.com>
2022-03-26 15:03:30 +0530

    psyche build

--- config.sh
+++ config.sh
@@ -2,9 +2,9 @@
 
 # Device
 export FOX_BRANCH="fox_11.0"
-export DT_LINK="https://github.com/Pranav-Talmale/android_device_xiaomi_alioth-ofox.git -b fox_11.0"
+export DT_LINK="https://github.com/Pranav-Talmale/device_xiaomi_psyche-ofox"
 
-export DEVICE="alioth"
+export DEVICE="psyche"
 export OEM="xiaomi"
 export TARGET="adbd bootimage"
 
@@ -12,7 +12,7 @@ export OUTPUT="OrangeFox*.zip"
 
 # Kernel Source
 # Uncomment the next line if you want to clone a kernel source.
-export KERNEL_SOURCE="https://github.com/Pranav-Talmale/android_kernel_xiaomi_alioth"
+#export KERNEL_SOURCE="https://github.com/Pranav-Talmale/android_kernel_xiaomi_alioth"
 #export PLATFORM="sm6150" # Leave it commented if you want to clone the kernel to kernel/$OEM/$DEVICE
 
 # Not Recommended to Change
