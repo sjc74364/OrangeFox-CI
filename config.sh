9ca4a6973e4c76a1cda0aebc800bc7d069b090a3
sjc74364 <2918214206@qq.com>
2022-03-27 03:03:53 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,31 +1,47 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_11.0"
-export DT_LINK="https://github.com/Pranav-Talmale/device_xiaomi_psyche-ofox"
 
-export DEVICE="psyche"
+export FOX_BRANCH="fox_11.0"
+
+export DT_LINK="https://github.com/Pranav-Talmale/android_device_xiaomi_alioth-ofox.git -b fox_11.0"
+
+export DEVICE="alioth"
+
 export OEM="xiaomi"
+
 export TARGET="adbd bootimage"
 
 export OUTPUT="OrangeFox*.zip"
 
 # Kernel Source
+
 # Uncomment the next line if you want to clone a kernel source.
-#export KERNEL_SOURCE="https://github.com/Pranav-Talmale/android_kernel_xiaomi_alioth"
+
+export KERNEL_SOURCE="https://github.com/Pranav-Talmale/android_kernel_xiaomi_alioth"
+
 #export PLATFORM="sm6150" # Leave it commented if you want to clone the kernel to kernel/$OEM/$DEVICE
 
 # Not Recommended to Change
+
 export SYNC_PATH="$HOME/work" # Full (absolute) path.
+
 export USE_CCACHE=1
+
 export CCACHE_SIZE="50G"
+
 export CCACHE_DIR="$HOME/work/.ccache"
+
 export J_VAL=16
 
 if [ ! -z "$PLATFORM" ]; then
+
     export KERNEL_PATH="kernel/$OEM/$PLATFORM"
+
 else
+
     export KERNEL_PATH="kernel/$OEM/$DEVICE"
+
 fi
+
 export DT_PATH="device/$OEM/$DEVICE"
-#
